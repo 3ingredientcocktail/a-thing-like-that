e6f14c2e3b669869c03f977033ceae6b06c23157
Pablo Zamorano <3ingredientcocktail@gmail.com>
2026-06-03 14:46:21 -0400

    configure custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://3ingredientcocktail.github.io',
-  base: '/a-thing-like-that',
+  site: 'https://athinglikethat.blog',
   legacy: {
     collectionsBackwardsCompat: true,
   },
